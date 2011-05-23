208273c13913ac5285b398b6d459e2cb82555808
Michael Farmer <mikefarmer@Michael-Farmers-iMac.local>
2011-05-23 14:30:45 -0600

    updated setup script

--- setup_links.sh
+++ setup_links.sh
@@ -1,7 +1,14 @@
+mv ~/.bashrc ~/.bashrc_original
 ln -s ~/tmp/dotfiles/bashrc ~/.bashrc
+mv ~/.gemrc ~/.gemrc_original
 ln -s ~/tmp/dotfiles/gemrc ~/.gemrc
+mv ~/.gitconfig ~/.gitconfig_original
 ln -s ~/tmp/dotfiles/gitconfig ~/.gitconfig
+mv ~/.screenrc ~/.screenrc_original
 ln -s ~/tmp/dotfiles/screenrc ~/.screenrc
+mv ~/.vimrc ~/.vimrc_original
 ln -s ~/tmp/dotfiles/vimrc ~/.vimrc
+mv ~/.vim ~/.vim_original
 ln -s ~/tmp/dotfiles/vim ~/.vim
+
 cp ~/tmp/dotfiles/default_bash ~/.default_bash
