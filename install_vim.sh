1523a6f52eb9dc8d5024854d1f1a9f7ee51ffad1
Michael Farmer <mikefarmer@Michael-Farmers-iMac.local>
2011-05-23 15:13:23 -0600

    more vim install fixes

--- install_vim.sh
+++ install_vim.sh
@@ -2,8 +2,9 @@ cd ~/tmp
 wget ftp://ftp.vim.org/pub/vim/unix/vim-7.3.tar.bz2
 tar -jxvf vim-7.3.tar.bz2
 cd vim73
-configure ./configure --enable-rubyinterp --prefix=$HOME/tmp/custom_vim
+./configure --enable-rubyinterp --prefix=$HOME/tmp/custom_vim
 make && make install
+cd ..
 rm -rf vim73
 rm vim-7.3.tar.bz2
 cd ~/tmp/dotfiles
