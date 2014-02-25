13f735c41cb8c14eb8bcd613da7c952c6358b4a7
Michael Farmer <mike.farmer@gmail.com>
2014-02-25 16:25:11 -0700

    Use VIM 7.4

--- install_vim.sh
+++ install_vim.sh
@@ -2,12 +2,12 @@
 set -e
 
 cd ~/tmp
-wget ftp://ftp.vim.org/pub/vim/unix/vim-7.3.tar.bz2
-tar -jxvf vim-7.3.tar.bz2
-cd vim73
+wget ftp://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2
+tar -jxvf vim-7.4.tar.bz2
+cd vim74
 ./configure --enable-rubyinterp --enable-pythoninterp --prefix=$HOME/tmp/custom_vim --with-features=big
 make && make install
 cd ..
-rm -rf vim73
-rm vim-7.3.tar.bz2
+rm -rf vim74
+rm vim-7.4.tar.bz2
 cd ~/tmp/dotfiles
